45d2fb6b2acc800133c1f1f5f3fea5a6495b531d
Adssx-dev <adssx.dev@gmail.com>
2023-10-30 20:00:59 +0100

    Solving year 2020 day 4 part 2

--- src/2020/Day4.hs
+++ src/2020/Day4.hs
@@ -1,17 +1,45 @@
 import System.IO
 import Debug.Trace
+import Data.List
 
 
 main = do
     handle <- openFile "data/2020/Day4.txt" ReadMode
     contents <- hGetContents handle
     let passports = parsePassports $ lines contents
-    print $ length $ filter id $ map validatePassports passports
+    print $ length $ filter id $ map validatePassportsPart1 passports
+    print $ length $ filter id $ map validatePassportsPart2 passports
 
     hClose handle
 
+validatePassportsPart1 :: [[[Char]]] -> Bool
 validatePassportsPart1 pass = length (concat $ filter (/="cid") $ map head pass) == 21
 
+validatePassportsPart2 :: [[[Char]]] -> Bool
+validatePassportsPart2 pass = all validatePassportField pass && validatePassportsPart1 pass
+
+validatePassportField :: [String] -> Bool
+validatePassportField ["byr", yearStr] = validateInBounds yearStr 1920 2002
+validatePassportField ["iyr", yearStr] = validateInBounds yearStr 2010 2020
+validatePassportField ["eyr", yearStr] = validateInBounds yearStr 2020 2030
+validatePassportField ["hgt", height] = case unit of
+        "cm" -> validateInBounds value 150 193
+        "in" -> validateInBounds value 59 76
+        _ -> False
+    where (value, unit) = partition (`elem` ['0'..'9']) height
+validatePassportField ["hcl", '#':color] = all (`elem` ['a'..'f'] ++ ['0'..'9']) color && length color == 6
+validatePassportField ["hcl", _] = False
+validatePassportField ["ecl", color] = color `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+validatePassportField ["pid", id] = all (`elem` ['0'..'9']) id && length id == 9
+validatePassportField ["cid", _] = True
+validatePassportField _ = False
+
+
+validateInBounds valueStr min max = value >= min && value <= max
+    where value = read valueStr
+
+
+parsePassports :: [[Char]] -> [[[[Char]]]]
 parsePassports textLines = map parseOnePassport passports
     where
         passports = splitOn (== []) textLines
