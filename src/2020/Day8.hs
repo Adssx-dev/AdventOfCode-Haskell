e16e3a3a63a1b7cbf0ee9f9013adee3f62e496e3
Adssx-dev <adssx.dev@gmail.com>
2023-11-10 12:45:25 +0100

    Solving year 2020 day 8 part 2

--- src/2020/Day8.hs
+++ src/2020/Day8.hs
@@ -3,6 +3,7 @@ import Debug.Trace
 import Data.List
 import System.Directory (Permissions(executable))
 import qualified Data.Set as Set
+import qualified Data.Maybe as Maybe
 
 
 
@@ -11,18 +12,55 @@ main = do
     contents <- hGetContents handle
 
     let program = map parseLine (lines contents)
-    print $ executeProgram program Set.empty 0 0
+    print $ executeProgramPt1 program Set.empty 0 0
+
+    print $ findRightProgram program 0
     hClose handle
 
-executeProgram :: [(String, Int -> Int)] -> Set.Set Int -> Int -> Int -> Int
-executeProgram program history accumulator currentLine = result
+
+alterProgram program targetInstruction = result
+    where
+        newInstruction = alterInstruction $ program !! targetInstruction
+        (before, _:after) = splitAt targetInstruction program
+        result = case newInstruction of 
+            Just i -> Just $ before ++ i: after
+            Nothing -> Nothing
+
+
+findRightProgram initialProgram instructionToAlter = result
+    where
+        alteredProgram = alterProgram initialProgram instructionToAlter
+        result = case alteredProgram of
+            Nothing -> findRightProgram initialProgram (instructionToAlter + 1)
+            Just newProgram -> case executeProgramPt2 (Maybe.fromMaybe [] alteredProgram) Set.empty 0 0 of 
+                Nothing -> findRightProgram initialProgram (instructionToAlter + 1)
+                Just res -> res
+
+
+alterInstruction ("nop", fcn) = Just ("jmp", fcn)
+alterInstruction ("jmp", fcn) = Just ("nop", fcn)
+alterInstruction (op, fcn) = Nothing
+
+executeProgramPt2 :: [(String, Int -> Int)] -> Set.Set Int -> Int -> Int -> Maybe Int
+executeProgramPt2 program history accumulator currentLine = result
+    where
+        (newAccumulator, newLine) = executeLine (program !! currentLine) (accumulator, currentLine)
+        hasReachedEnd = currentLine == length program
+        result = case (hasReachedEnd, Set.member currentLine history) of
+            (True, _) -> Just accumulator
+            (_, True) -> Nothing
+            (_, False) -> executeProgramPt2 program (Set.insert currentLine history) newAccumulator newLine
+                
+
+executeProgramPt1 :: [(String, Int -> Int)] -> Set.Set Int -> Int -> Int -> Int
+executeProgramPt1 program history accumulator currentLine = result
     where
         (newAccumulator, newLine) = executeLine (program !! currentLine) (accumulator, currentLine)
         result = if Set.member currentLine history 
             then 
                 accumulator 
             else 
-                executeProgram program (Set.insert currentLine history) newAccumulator newLine
+                executeProgramPt1 program (Set.insert currentLine history) newAccumulator newLine
 
 
 executeLine :: (String, Int -> Int) -> (Int, Int) -> (Int, Int)
