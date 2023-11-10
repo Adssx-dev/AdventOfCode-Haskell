a501868891a76b2d54701b363812c50cdbbbe311
Adssx-dev <adssx.dev@gmail.com>
2023-11-10 20:29:05 +0100

    Solving year 2020 day 9 part 2

--- src/2020/Day9.hs
+++ src/2020/Day9.hs
@@ -15,11 +15,35 @@ main = do
     let (preamble, numbers) = splitAt 25 allNumbers
     -- Part 1
     -- function expects preamble to be reversed !
-    print $ validateList numbers (reverse preamble)
+    let invalidNumber = validateList numbers (reverse preamble)
+    print invalidNumber
 
+    -- Part 2
 
+    print $  part2 allNumbers invalidNumber
 
     hClose handle
+
+part2 :: [Int] -> Int -> Int
+part2 list target = minimum resultList + maximum resultList
+    where
+        result = testAllWindowsToGetTarget list 2 target
+        resultList = snd $ head result
+
+testAllWindowsToGetTarget list windowSize target = result
+    where 
+        result = case findContiguousThatSumToTarget list windowSize target of
+            [] -> testAllWindowsToGetTarget list (windowSize + 1) target
+            x -> x
+
+findContiguousThatSumToTarget list windowSize target = [(sum window, window) | window <- windows, sum window == target]--zip (map sum windows) windows
+    where
+        windows = movingWindow list windowSize
+
+movingWindow list width = if width >= length list then
+            []
+        else
+            take width list : movingWindow (tail list) width
 
 validateList :: [Int] -> [Int] -> Int
 validateList (currentElem:remaining) lastDigits = if found then validateList remaining (currentElem:take 25 lastDigits) else currentElem
