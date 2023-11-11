76d129fc99388fa7af9f11dab6aff0c0aa4d0054
Adssx-dev <adssx.dev@gmail.com>
2023-11-11 18:14:08 +0100

    Solving year 2020 day 10 part 2

--- src/2020/Day10.hs
+++ src/2020/Day10.hs
@@ -8,9 +8,24 @@ main = do
     contents <- hGetContents handle
 
     print $ part1 $ lines contents
+    print $ part2 $ lines contents
 
     hClose handle
 
+
+-- Only the number of successives ones matter for each group of ones
+-- Number of possible path depending on the number of successive ones :
+-- 1 -> 1
+-- 2 -> 2
+-- 3 -> 4
+-- 4 -> 7
+-- More -> IDK
+part2 lines = product products
+    where
+        intialNumberList = nub $ sort $ map read lines :: [Int]
+        numberList = 0 : intialNumberList ++  [maximum intialNumberList + 3]
+        differences = map length $ filter (\x -> head x == 1) $ group $  zipWith (\ a b -> b - a) (init numberList) (tail numberList)
+        products = map (\x -> [1, 2, 4, 7] !! (x-1)) differences
 
 part1 lines = product differences
     where
