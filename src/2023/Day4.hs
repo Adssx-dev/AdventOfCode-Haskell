bd68ebb2fe02b17873f8da4708511a11941370b3
Adssx-dev <adssx.dev@gmail.com>
2023-12-06 16:10:29 +0100

    Solving year 2023 day 4 part 2

--- src/2023/Day4.hs
+++ src/2023/Day4.hs
@@ -4,36 +4,51 @@ import Data.List
 import Data.Function
 import Data.Maybe
 import Data.Char
-import Language.Haskell.TH (newtypeStrategy)
 
 
-data Card = Card {number :: Int, winnings :: [Int], numbers :: [Int]} deriving (Show, Eq, Ord)
+data Card = Card {number :: Int, winnings :: [Int], numbers :: [Int], value :: Int} deriving (Show, Eq, Ord)
 
 main = do
     handle <- openFile "data/2023/Day4.txt" ReadMode
     contents <- hGetContents handle
 
-    --print $ map parseCard $ lines contents
     print $ part1 $ lines contents
-    --print $ part2 $ lines contents
+
+    let cards = map parseCard $ lines contents
+    print $ part2 cards (replicate (length cards) 1)
 
     hClose handle
+
+part2 :: [Card] -> [Int] -> Int
+part2 [] _ = 0
+part2 (currentCard:cards) (amount:amounts) =  amount + part2 cards amountsUpdated
+    where
+        currentCardScore =  cardScorePt2 currentCard
+        earnings = replicate currentCardScore ((* amount) 1)
+        amountsUpdated =  map sum $ transpose [amounts, earnings]
+
 
 part1 lines = sum $ map cardScore cards
     where
         cards = map parseCard lines
 
+cardScorePt2 :: Card -> Int
+cardScorePt2 card = numberOfWinningNumbers
+    where
+        winningNumbers = filter (`elem` winnings card) (numbers card)
+        numberOfWinningNumbers = length winningNumbers
+
 cardScore :: Card -> Int
 cardScore card = score
-    where 
+    where
         winningNumbers = filter (`elem` winnings card) (numbers card)
         numberOfWinningNumbers = length winningNumbers
         score = if numberOfWinningNumbers == 0
             then 0
             else 2 ^ (numberOfWinningNumbers - 1)
 
-parseCard line = Card{number=cardNumber, winnings=winningNumbers, numbers=cardNumbers}
-    where 
+parseCard line = Card{number=cardNumber, winnings=winningNumbers, numbers=cardNumbers, value = 0}
+    where
         [cardPart, numbersPart] = splitOn (==':') line
         [_, cardNumberStr] = words cardPart
         cardNumber = read cardNumberStr :: Int
