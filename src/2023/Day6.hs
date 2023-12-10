a7e31c36cc7434ade3a7a685bd9f9a05e46dbca3
Adssx-dev <adssx.dev@gmail.com>
2023-12-10 21:01:46 +0100

    Solving year 2023 day 6 part 1,n part 2 almost ok

---
+++ src/2023/Day6.hs
@@ -0,0 +1,43 @@
+import System.IO
+import Debug.Trace
+import Data.List
+import Data.Function
+import Data.Maybe
+import Data.Char
+
+
+
+
+main = do
+    handle <- openFile "data/2023/Day6.txt" ReadMode
+    contents <- hGetContents handle
+
+    print $ part1 (lines contents)
+    print $ part2 (lines contents)
+
+
+
+    hClose handle
+
+part2 :: [String] -> Int
+part2 [timesStr, distancesStr] = result
+    where
+        time = read (concat $ tail $ words timesStr) :: Float
+        distance = read (concat $ tail $ words distancesStr) :: Float
+        result =  numberOfIntegerQradraticSolutions (-1) time (-distance - 0.00000000001)
+
+part1 :: [String] -> Int
+part1 [timesStr, distancesStr] = product possibilities
+    where
+        times = map read (tail $ words timesStr) :: [Int]
+        distances = map read (tail $ words distancesStr) :: [Int]
+        zipped = zip (map fromIntegral times :: [Float]) (map fromIntegral distances :: [Float])
+        possibilities = map (\(t, d) -> numberOfIntegerQradraticSolutions (-1) t (-d - 0.001)) zipped
+
+numberOfIntegerQradraticSolutions :: Float -> Float -> Float -> Int
+numberOfIntegerQradraticSolutions a b c =  floor largeRoot - ceiling smallRoot + 1
+    where
+        delta = b * b - 4 * a * c
+        roots = [-(b - sqrt delta)/(2*a), -(b + sqrt delta)/(2*a)]
+        smallRoot = minimum roots
+        largeRoot = maximum roots
