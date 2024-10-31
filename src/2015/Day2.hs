268cb5bbbaebaed87d32f3c9035b8cbe873ac9cc
Adssx-dev <adssx.dev@gmail.com>
2024-10-31 12:35:30 +0100

    Solving year 2015 day 2 part 2

--- src/2015/Day2.hs
+++ src/2015/Day2.hs
@@ -9,7 +9,7 @@ main = do
     contents <- hGetContents handle
 
     print $ part1 contents
-    --print $ part2  contents
+    print $ part2 contents
 
     hClose handle
 
@@ -17,13 +17,19 @@ part1 :: [Char] -> Int
 part1 inputStr = sum $ map (computeArea . parsePresents) $ lines inputStr
 
 
---part2 :: [Char] -> Int
---part2 inputStr = 1 + length (takeWhile (>=0) $ scanl1 (+) $ map convertParenthesis inputStr)
+part2 :: [Char] -> Int
+part2 inputStr = sum $ map (computeRibbonLength . parsePresents) $ lines inputStr
 
 computeArea :: (Int, Int, Int) -> Int
 computeArea (l, w, h) = minimum sides + 2 * sum sides 
     where
         sides = [l * w, w * h, h * l]
+
+computeRibbonLength :: (Int, Int, Int) -> Int
+computeRibbonLength (l, w, h) = volume + minimum perimeters
+    where
+        perimeters = [2*l + 2*w, 2*w + 2*h, 2*h + 2*l]
+        volume = l * w * h
 
 parsePresents :: [Char] -> (Int, Int, Int)
 parsePresents sizeStr = (read (splitted !! 0) :: Int, read (splitted !! 1) :: Int, read (splitted !! 2) ::Int)
