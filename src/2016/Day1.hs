7ba767b1cb54769bc88d9a4bbe692189b6c1f0bf
Adssx-dev <adssx.dev@gmail.com>
2024-11-03 18:28:00 +0100

    Solving year 2017 day 1 part 1&2

--- src/2016/Day1.hs
+++ src/2016/Day1.hs
@@ -2,6 +2,7 @@ import System.IO
 import Debug.Trace
 import Data.List
 import Data.Char
+import Data.Set
 
 data Coordinates = Coordinates {row :: Int, col :: Int} deriving (Show, Eq, Ord)
 
@@ -9,8 +10,8 @@ main = do
     handle <- openFile "data/2016/Day1.txt" ReadMode
     contents <- hGetContents handle
 
-    print $ part1 contents
-    --print $ part2  contents
+    --print $ part1 contents
+    print $ part2 "R8, R4, R4, R8" -- contents
 
     hClose handle
 
@@ -19,17 +20,30 @@ part1 inputStr = abs(row position) + abs(col position)
     where
         position = executeAll (words inputStr) Coordinates{row=0,col=0} Coordinates{row=0, col=1}
 
+part2 :: [Char] -> Int
+part2 inputStr = abs(row position) + abs(col position)
+    where
+        position = executeAllFindVisited (words inputStr) Coordinates{row=0,col=0} Coordinates{row=0, col=1} $ Data.Set.fromList [Coordinates{row=0,col=0}]
+
+
 executeAll [] position _ = position
 executeAll (c:commands) position direction =  executeAll commands newPosition newDirection
     where
         (newPosition, newDirection) = executeCommand c position direction
 
--- part2 :: [Char] -> Int
--- part2 inputStr = 1 + length (takeWhile (>=0) $ scanl1 (+) $ map convertParenthesis inputStr)
+executeAllFindVisited :: [[Char]] -> Coordinates -> Coordinates -> Set Coordinates -> Coordinates
+executeAllFindVisited [] _ _ _ = error "Reached end without visiting spot twice"
+executeAllFindVisited (c:commands) position direction visited = if Data.Set.member newPosition visited
+            then newPosition
+            else trace (show (newPosition, visited))  executeAllFindVisited commands newPosition newDirection allVisited
+    where
+        (newPosition, newDirection) = executeCommand c position direction
+        allVisited = Data.Set.insert newPosition visited
+
 
 executeCommand (dirMod:length) coords currentDirection = (iterate (addCoordinates newDirection) coords !! n, newDirection)
     where
-        n = read (filter isDigit length) :: Int -- parse count with maybe trailing coma
+        n = read (Data.List.filter isDigit length) :: Int -- parse count with maybe trailing coma
         newDirection = if dirMod == 'L' then turnLeft currentDirection else turnRight currentDirection
 
 -- Coordinates are used as vectors in 2-D space
