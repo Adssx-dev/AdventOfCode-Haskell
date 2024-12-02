b94ad779c31b42a2561b1f46a0232ac42e2f9a8c
Adssx-dev <adssx.dev@gmail.com>
2024-12-02 19:25:10 +0100

    Solving year 2024 day 2 part 1 & 2

--- src/App/Solutions/Y2024/Day2.hs
+++ src/App/Solutions/Y2024/Day2.hs
@@ -9,8 +9,23 @@ import Data.List
 
 
 part1 :: [Char] -> Maybe Int
-part1 inputStr = Nothing
+part1 inputStr = Just $ length $ filter validateReport $ parseReport inputStr
 
 
 part2 :: [Char] -> Maybe Int
-part2 inputStr = Nothing
+part2 inputStr = Just $ length $ filter (any validateReport . genListOneElementMissing) $ parseReport inputStr 
+
+parseReport inputStr = map (map read . words) $ lines inputStr :: [[Int]]
+
+validateReport report = (areAllIncreasing || areAllDecreasing) && areAtMost3 && areAtLeast1
+    where
+        diff = zipWith (-) (tail report) report
+        areAllIncreasing = all (>0) diff
+        areAllDecreasing = all (<0) diff
+        areAtMost3 = all ((<= 3) . abs) diff
+        areAtLeast1 = all ((>= 1) . abs) diff
+
+genListOneElementMissing [x] = [[x], []]
+genListOneElementMissing (x:xs) = map (x:) recurse ++ [head recurse]
+    where 
+        recurse = genListOneElementMissing xs
