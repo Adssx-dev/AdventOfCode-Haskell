aab676c24f991373009adadb056b290418bca39e
Adssx-dev <adssx.dev@gmail.com>
2024-12-19 17:53:36 +0100

    Solving year 2024 day 13 part 2

--- src/App/Solutions/Y2024/Day13.hs
+++ src/App/Solutions/Y2024/Day13.hs
@@ -8,12 +8,13 @@ import System.IO
 import Debug.Trace
 import Data.List
 import Utils.Matrix
+import Utils.Geometry
 
-data Machine = Machine{mat::Matrix2, vec :: Vector2f}
+data Machine = Machine{mat::Matrix2, vec :: Vector}
 
 part1 :: [Char] -> Maybe Int
 part1 inputStr = Just $ sum $ map tokenCost machines
-    where 
+    where
         machines = parse (lines inputStr) []
 
 parse :: [String] -> [Machine] -> [Machine]
@@ -21,8 +22,8 @@ parse [] machines = machines
 parse ([]:xs) machines = parse xs machines
 parse (a:b:prize:xs) machines = parse xs (Machine{mat=mat, vec=vec}:machines)
     where
-        readX str = read $ signFilter $ takeWhile (/= ',') $ drop 1 $ dropWhile (/= 'X') str :: Double
-        readY str = read $ signFilter $ drop 1 $ dropWhile (/= 'Y') str :: Double
+        readX str = read $ signFilter $ takeWhile (/= ',') $ drop 1 $ dropWhile (/= 'X') str :: Int
+        readY str = read $ signFilter $ drop 1 $ dropWhile (/= 'Y') str :: Int
         signFilter = filter (\x -> x /= '+' && x /= '=')
         ax = readX a
         ay = readY a
@@ -31,30 +32,15 @@ parse (a:b:prize:xs) machines = parse xs (Machine{mat=mat, vec=vec}:machines)
         tx = readX prize
         ty = readY prize
         mat = Matrix2{a = ax, b = bx, c = ay, d = by}
-        vec = Vector2f{dx = tx, dy = ty}
+        vec = Vector{dx = tx, dy = ty}
 
--- trace (show (a, b)) $
-tokenCost :: Machine -> Int
-tokenCost Machine{..} =  trace (show (a, b)) $ if isInt a && isInt b
-    then 3 * round a + round b
-    else 0
-    where
-        solution = mul (inv mat) vec
-        a = dx solution
-        b = dy solution
+tokenCost Machine{..} = case solveLinearEquation mat vec of
+    Nothing -> 0
+    Just sol -> dx sol * 3 + dy sol
 
--- isSolution Matix2{..} Vector2f{..} = 
---     where
---         dxInt = round
-
-isInt :: Double -> Bool
-isInt x = isIntInt x 1
-
-isIntInt :: (Integral a, RealFrac b) => b -> a -> Bool
-isIntInt x n = (round $ 10^(fromIntegral n)*(x-(fromIntegral $ round x)))==0
 
 part2 :: [Char] -> Maybe Int
-part2 inputStr = trace (show $ map tokenCost machinesFurther) $  Just $ sum $ map tokenCost machinesFurther
-    where 
+part2 inputStr =  Just $ sum $ map tokenCost machinesFurther
+    where
         machines = parse (lines inputStr) []
-        machinesFurther = map (\Machine{..} -> Machine{mat=mat, vec=Vector2f{dx=(dx vec)+10000000000000, dy = (dy vec)+10000000000000}}) machines
+        machinesFurther = map (\Machine{..} -> Machine{mat=mat, vec=Vector{dx=dx vec + 10000000000000, dy = dy vec + 10000000000000}}) machines
--- src/App/Utils/Matrix.hs
+++ src/App/Utils/Matrix.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
-module Utils.Matrix 
+module Utils.Matrix
 where
 
 import Data.List
@@ -12,29 +12,27 @@ import Utils.Geometry
 -- | a b |
 -- | c d |
 data Matrix2 = Matrix2
-    { a :: Double
-    , b :: Double
-    , c :: Double
-    , d :: Double
+    { a :: Int
+    , b :: Int
+    , c :: Int
+    , d :: Int
     } deriving (Eq, Show, Ord)
 
-data Vector2f = Vector2f
-    { dx :: Double
-    , dy :: Double
-    } deriving (Eq, Show, Ord)
 
-mul :: Matrix2 -> Vector2f -> Vector2f
-mul Matrix2{..} Vector2f{..} = Vector2f{dx = a * dx + b *  dy, dy = c * dx + d *  dy}
+mul :: Matrix2 -> Vector -> Vector
+mul Matrix2{..} Vector{..} = Vector{dx = a * dx + b *  dy, dy = c * dx + d *  dy}
 
-inv :: Matrix2 -> Matrix2
-inv Matrix2{..} = Matrix2
-    { a =  d * det
-    , b = -b * det
-    , c = -c * det
-    , d =  a * det
-    }
-    where 
-        det = 1 / (a * d - b * c)
+solveLinearEquation :: Matrix2 -> Vector -> Maybe Vector
+solveLinearEquation mat target = if mul mat candidate == target || det == 0
+        then Just candidate
+        else Nothing
+    where
+        det = determinant mat
+        candidate = Vector
+            {dx= (d mat * dx target - b mat * dy target) `div` det
+            ,dy= (-c mat * dx target + a mat * dy target) `div` det}
+
+determinant Matrix2{..} = a * d - b * c
 
 -- Get all the diagonals of a matrix perpendicular to principal diagonal
 -- Transpose the matrix to get the other diagonals
@@ -47,7 +45,7 @@ toCoordinateMap :: [[a]] -> Map.Map Point a
 toCoordinateMap mat = Map.fromList $ toCoordinateList mat
 
 toCoordinateList :: [[a]] -> [(Point, a)]
-toCoordinateList mat = concat $ zipWith 
-            (\row lst -> map (\(col, value) -> (Point{x=col, y=row}, value)) lst) 
+toCoordinateList mat = concat $ zipWith
+            (\row lst -> map (\(col, value) -> (Point{x=col, y=row}, value)) lst)
             [0..] -- row coordinates
             (map (zip [0..]) mat) -- col coordinate + value
