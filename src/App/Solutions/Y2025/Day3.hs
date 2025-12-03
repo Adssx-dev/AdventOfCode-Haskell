bf12335525c0ecf8a399106fc83db2610fb0336f
Adssx-dev <adssx.dev@gmail.com>
2025-12-03 11:31:04 +0100

    Solving year 2025 day 3 part 1

--- src/App/Solutions/Y2025/Day3.hs
+++ src/App/Solutions/Y2025/Day3.hs
@@ -6,11 +6,29 @@ module Solutions.Y2025.Day3
 import System.IO
 import Debug.Trace
 import Data.List
+import Data.Char (digitToInt)
 
 
 part1 :: [Char] -> Maybe Int
-part1 inputStr = Nothing
+part1 inputStr = trace (show $ map solveBank banks) Just $ sum $ map solveBank banks
+    where
+        banks = parseInput inputStr
 
+solveBank (val1:val2:remaining) = bestPair val1 val2 (val2:remaining)
+
+bestPair :: Int -> Int -> [Int] -> Int
+bestPair digit1 digit2 [] = digit1 * 10 + digit2
+bestPair digit1 digit2 [val] = digit1 * 10 + max digit2 val
+bestPair digit1 digit2 (val1:val2:remaining)
+  | val1 > digit1 = bestPair val1 val2 (val2:remaining)
+  | val1 > digit2 = bestPair digit1 val1 (val2:remaining)
+  | otherwise = bestPair digit1 digit2 (val2:remaining)
+
+parseInput :: String -> [[Int]]
+parseInput = map parseBank . lines
+
+parseBank :: String -> [Int]
+parseBank b = map digitToInt b :: [Int]
 
 part2 :: [Char] -> Maybe Int
 part2 inputStr = Nothing
