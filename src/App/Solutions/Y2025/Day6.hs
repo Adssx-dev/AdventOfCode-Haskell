1b1ae5fdce72b029950ce4c26d13038d6795f930
Adssx-dev <adssx.dev@gmail.com>
2025-12-06 11:05:18 +0100

    Solving year 2025 day 6 part 2

--- src/App/Solutions/Y2025/Day6.hs
+++ src/App/Solutions/Y2025/Day6.hs
@@ -6,34 +6,53 @@ module Solutions.Y2025.Day6
 import System.IO
 import Debug.Trace
 import Data.List
+import Utils.List
 
 
 type Operand = Int
 type Operator = (Int -> Int -> Int)
 
-parseInput :: String -> ([[Operand]], [Operator])
-parseInput inputStr = (map (map read . words) operandLines :: [[Operand]], map parseOperator $ words operatorLine)
+-- Parsing
+parseInputPart1 :: String -> ([[Operand]], [Operator])
+parseInputPart1 inputStr = (map (map read . words) operandLines :: [[Operand]], map parseOperator $ words operatorLine)
     where
         lns = lines inputStr
         operandLines = init lns
         operatorLine = last lns
         parsedOperands = map words operandLines
 
+
+
+parseInputPart2 :: String -> [([Operand], Operator)]
+parseInputPart2 inputStr = map parseOneLinePt2 cleaned
+    where
+        reordered = reverse $ transpose $ lines inputStr
+        grouped = splitOn isStringBlank reordered
+        cleaned =  map (concatMap (filter (not.isStringBlank) . (\g -> [init g, [last g]]))) grouped
+
+parseOneLinePt2 :: [String] -> ([Operand], Operator)
+parseOneLinePt2 strings = (operands, operator)
+    where
+        operands = map read (init strings) :: [Operand]
+        operator = parseOperator $ last strings
+
 parseOperator "+" = (+)
 parseOperator "*" = (*)
 
+isStringBlank :: String -> Bool
+isStringBlank = all (==' ')
+
+-- Solving
 solveOneProblem :: [Operand] -> Operator -> Int
 solveOneProblem operands operator = foldl1 operator operands
 
 part1 :: [Char] -> Maybe Int
 part1 inputStr = Just $ sum res
     where
-        (operands, operators) = parseInput inputStr
+        (operands, operators) = parseInputPart1 inputStr
         res = zipWith solveOneProblem (transpose operands) operators
 
-
-
-
-
 part2 :: [Char] -> Maybe Int
-part2 inputStr = Nothing
+part2 inputStr = Just $ sum $ map (uncurry solveOneProblem) parsed
+    where
+        parsed = parseInputPart2 inputStr
