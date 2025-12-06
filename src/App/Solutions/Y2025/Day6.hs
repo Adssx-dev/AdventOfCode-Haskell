ac094a634f3d5613f09d06631b1818b4c5131b30
Adssx-dev <adssx.dev@gmail.com>
2025-12-06 10:49:11 +0100

    Solving year 2025 day 6 part 1

--- src/App/Solutions/Y2025/Day6.hs
+++ src/App/Solutions/Y2025/Day6.hs
@@ -8,8 +8,31 @@ import Debug.Trace
 import Data.List
 
 
+type Operand = Int
+type Operator = (Int -> Int -> Int)
+
+parseInput :: String -> ([[Operand]], [Operator])
+parseInput inputStr = (map (map read . words) operandLines :: [[Operand]], map parseOperator $ words operatorLine)
+    where
+        lns = lines inputStr
+        operandLines = init lns
+        operatorLine = last lns
+        parsedOperands = map words operandLines
+
+parseOperator "+" = (+)
+parseOperator "*" = (*)
+
+solveOneProblem :: [Operand] -> Operator -> Int
+solveOneProblem operands operator = foldl1 operator operands
+
 part1 :: [Char] -> Maybe Int
-part1 inputStr = Nothing
+part1 inputStr = Just $ sum res
+    where
+        (operands, operators) = parseInput inputStr
+        res = zipWith solveOneProblem (transpose operands) operators
+
+
+
 
 
 part2 :: [Char] -> Maybe Int
