5eb914405a7fef5f4d0c60b747bc88eac8a3c776
Adssx-dev <adssx.dev@gmail.com>
2025-12-07 14:56:58 +0100

    Solving year 2025 day 7 part 2

--- src/App/Solutions/Y2025/Day7.hs
+++ src/App/Solutions/Y2025/Day7.hs
@@ -21,7 +21,6 @@ parseCell '.' = Empty
 -- Simulation
 
 simulation :: [Point] -> Set.Set Point -> ([Point], Int)
---simulation beams splitters = until (\b -> trace (show (lastSplitterYPos, b)) lastSplitterYPos < y (head $ fst b)) step (beams, 0)
 simulation beams splitters = until (\b ->  lastSplitterYPos < y (head $ fst b)) step (beams, 0)
     where
         lastSplitterYPos = Set.findMax $ Set.map y splitters
@@ -38,6 +37,24 @@ advanceBeam splitters beam = if Set.member beam splitters
         rightBeamPos = Point{x=x newBeamPos + 1, y=y newBeamPos}
 
 
+simulationPt2 :: [(Point, Int)] -> Set.Set Point -> ([(Point, Int)], Int)
+simulationPt2 beams splitters = until (\b ->  lastSplitterYPos < y (fst $ head $ fst b)) step (beams, 0)
+    where
+        lastSplitterYPos = Set.findMax $ Set.map y splitters
+        step b = foldl (\(op, oc) (np, nc) -> (mergeBeamsPt2 op ++ np, oc + nc )) ([], snd b) $ map (advanceBeamPt2 splitters) (fst b)
+
+mergeBeamsPt2 :: [(Point, Int)] -> [(Point, Int)]
+mergeBeamsPt2 beams = map (\e -> (fst $ head e, sum $ map snd e)) $ groupBy (\a b -> fst a == fst b) beams
+
+advanceBeamPt2 :: Set.Set Point -> (Point, Int) -> ([(Point, Int)], Int)
+advanceBeamPt2 splitters (beam, beamCount) = if Set.member beam splitters
+    then ([(leftBeamPos, beamCount), (rightBeamPos, beamCount)], beamCount)
+    else ([(newBeamPos, beamCount)], 0)
+    where
+        newBeamPos = Point{x=x beam, y=y beam + 1}
+        leftBeamPos = Point{x=x newBeamPos - 1, y=y newBeamPos}
+        rightBeamPos = Point{x=x newBeamPos + 1, y=y newBeamPos}
+
 part1 :: [Char] -> Maybe Int
 part1 inputStr = Just $ snd $ simulation [initialPoint] splittersPos
     where
@@ -47,6 +64,11 @@ part1 inputStr = Just $ snd $ simulation [initialPoint] splittersPos
         splittersPos = Set.fromList $ map fst $ M.toList $ M.filter (== Splitter) parsed
 
 
-
+-- Why a +1 ? I have no idea but it works both on example data and on actual input
 part2 :: [Char] -> Maybe Int
-part2 inputStr = Nothing
+part2 inputStr = Just $ 1 + snd (simulationPt2 [(initialPoint, 1)] splittersPos)
+    where
+        parsed = parseInputToMap2d parseCell inputStr
+        sourcePos = fst $ head $ M.toList $ M.filter (== Source) parsed
+        initialPoint = Point{x=x sourcePos, y= y sourcePos + 1}
+        splittersPos = Set.fromList $ map fst $ M.toList $ M.filter (== Splitter) parsed
